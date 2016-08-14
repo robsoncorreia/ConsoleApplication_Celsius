d168ac025764dd49b5f16a0470240e6d32e9b25d
Robson Correia <robsoncorreia01@outlook.com>
2016-08-14 18:46:55 -0300

    sim

--- ConsoleApplication_Celsius/Program.cs
+++ ConsoleApplication_Celsius/Program.cs
@@ -10,6 +10,24 @@ namespace ConsoleApplication_Celsius
     {
         static void Main(string[] args)
         {
+            double f = -459.68f;
+            double c;
+
+            while (f < -459.67f || f > 1000f)
+            {
+                Console.WriteLine("Digite o valor em Fahrenheit:");
+                f = double.Parse(Console.ReadLine());
+
+                if (f < -459.67f || f > 1000f)
+                {
+                    Console.WriteLine("Valor invalido.");
+                }
+            }
+            c = (f - 32f) / 1.8f;
+
+            Console.WriteLine("{0:F2} Fahrenheit em Celsius é {1:F2}.", f, c);
+
+            Console.ReadKey();
         }
     }
 }
